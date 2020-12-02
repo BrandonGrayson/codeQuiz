e34f19f6d7941cf36b0f739b73f04c5556a3a3d3
Brandon Grayson <brandongrayson@Brandons-MacBook-Air.local>
2020-12-02 18:48:34 -0500

    Initial Commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// when the Start Quiz button is pressed
+
+// we want to take the user from the home screen to 
+
+// a page where they can see there first question
+
+// there should be a collection of possible answers either an array or an object
+
+// if a user selects a wrong answer to a question 
+
+// then we will subtract 10 seconds from time left 
+
+// display a message saying Incorrect to the user
+
+// else if the answer is correct 
+
