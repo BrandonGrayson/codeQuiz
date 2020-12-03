5f0d8a907c913df1da15d496a4502d8e6b3de4ae
Brandon Grayson <brandongrayson@Brandons-MacBook-Air.local>
2020-12-02 20:03:06 -0500

    console.logging h1 text content when start button is pressed

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
+// Select Variables
+let startBtn = document.querySelector('#start')
+let timerEl = document.querySelector('#timer')
+let titleEl = document.querySelector('#title')
+let h1Text = document.querySelector('#title').textContent
+
 // when the Start Quiz button is pressed
+startBtn.addEventListener('click', function () {
+    // console.log 
+    console.log('Quiz has been started')
+    // clear the text content of h1 element
+    h1Text.textContent = '';
+
+
+});
+// the timer should start at 60 and count down
+// we should display the first question to the user
+
+
 
 // we want to take the user from the home screen to 
 
@@ -6,11 +24,24 @@
 
 // there should be a collection of possible answers either an array or an object
 
-// if a user selects a wrong answer to a question 
+// if a user selects a correct answer to a question 
 
-// then we will subtract 10 seconds from time left 
+// diplay a message saying correct
+
+// timer continues to countdown as normal
+
+// a new question is loaded 
+
+// else if the user selects a wrong answer
+
+// then we will subtract 10 seconds from timer
 
 // display a message saying Incorrect to the user
 
-// else if the answer is correct 
+// then move onto next question
+
+// if the user answer all the questions 
+
+// then display all done! Final score is __, a textbox for their initials
+
 
