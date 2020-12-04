e8be26a7cd6e1f033c163a1c975fb73f8606e392
Brandon Grayson <brandongrayson@Brandons-Air.fios-router.home>
2020-12-04 07:13:21 -0500

    Correct answer is being displayed

--- script.js
+++ script.js
@@ -9,28 +9,14 @@ let questionAndAnswerCard = document.querySelector('#questionAndAnswerCard')
 let printQuestionEl = document.querySelector('#questionsWillGoHere')
 let printAnswerEl = document.querySelector('#answersGoHere')
 
-
 // Keep Track of Question =========
 let questionIndex = 0;
 // let hiddenEl = document.querySelector('#questionAndAnswerCard')
 
-
-// // access the time container div=================================================================
-// // let timeContainer = document.querySelector('#timeContainer')
-// // move the container to the top right corner
-// // timeContainer.style.position = 'absolute';
-// // timeContainer.style.top = 0;
-// // timeContainer.style.left = '90%';
-
 // // Timer variable =================================================================================
 // let timer = 5;
 
-// // after button is clicked 
-// // iterate through an array of questions
-// // select a question
-// // display that value to the screen
-// // 
-
+// the timer should start at 60 and count down
 // // An Array of Questions and Answers ===================================================================
 var questionsArray = [
     {
@@ -40,23 +26,67 @@ var questionsArray = [
     },
     {
         question: 'The condition in an if else statement is enclosed within?',
-        answers: ['Quotes','Curly Brackets','Parenthesis']
+        answers: ['Quotes','Curly Brackets','Parenthesis','Brackets'],
+        correctAnswer: 'Parenthesis'
     },
     {
         question: 'Arrays in javascript are used to store what?',
-        answers: ['Numbers and strings','Other arrays','booleans','All the above']
+        answers: ['Numbers and strings','Other arrays','booleans','All the above'],
+        correctAnswer: ''
     },
 ]
 
 // // when the Start Quiz button is pressed===============================================
-startBtn.addEventListener('click', function () {
+// call the start game function
+startBtn.addEventListener('click', startGame)  
+
+function startGame () {
     // console.log 
     console.log('Quiz has been started')
     // clear the text content of h1 element
-    // the timer should start at 60 and count down
     beginningScreen.setAttribute('style', 'display: none')
     questionAndAnswerCard.classList.remove('hidden')
-})  
+}
+
+
+// put question at the index of 0 on the page
+let questionHeadline = document.createElement('h1')
+questionHeadline.textContent = questionsArray[questionIndex].question
+printQuestionEl.appendChild(questionHeadline)
+
+
+// append the answers to the page
+// create the element they'll be in ul
+//loop over the answers / questionsArray[quesitonIndex].answers
+let possibleAnswers = document.getElementById('answers-container')
+
+let answersArray = questionsArray[questionIndex].answers
+let currentQuestion = questionsArray[questionIndex].question
+let correctAnswer = questionsArray[0].correctAnswer
+
+for (let i = 0; i < answersArray.length; i++) {
+    // create an element for the li data
+    let answerText = document.createElement('li')
+    //li data is answers array [i]
+    answerText.textContent = answersArray[i]
+    // give the answer text some styling classes
+    answerText.classList.add('answer-button')
+
+    answerText.addEventListener('click', function(event){
+        if (answerText.textContent === correctAnswer) {
+            console.log('Correct Answer!')
+        } else {
+            console.log('wrong answer')
+        }
+        event.stopPropagation()
+        console.log(event.target.innerHTML)
+    })
+    possibleAnswers.appendChild(answerText)
+}
+
+
+
+
 
 
 var setTimerText = function () {
@@ -69,28 +99,7 @@ var setTimerText = function () {
     // if statement to stop timer at 0
 }
 
-// put question at the index of 0 on the page
-let questionHeadline = document.createElement('h1')
-questionHeadline.textContent = questionsArray[questionIndex].question
-printQuestionEl.appendChild(questionHeadline)
-
-// append the answers to the page
-// create the element they'll be in ul
-
-//loop over the answers / questionsArray[quesitonIndex].answers
-let possibleAnswers = document.getElementById('answers-container')
-
-const answersArray = questionsArray[questionIndex].answers
-const currentQuestion = questionsArray[questionIndex].question
-
-for (let i = 0; i < answersArray.length; i++) {
-    // create an element for the li data
-    let answerText = document.createElement('li')
-    answerText.textContent = answersArray[i]
-    answerText.classList.add('answer-button')
-    answerText.addEventListener('click', function(event){
-        console.log(event.target.innerHTML)
-    })
-    possibleAnswers.appendChild(answerText)
-}
+// if the user selects the correct answer
+// console.log('correct ')
+// display a message saying correct 
 
